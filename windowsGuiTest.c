4b4c3e0ac47548b552834711ad824ac83e475c0f
AGhafoor94 <adnan_ghafoor94@hotmail.com>
2025-01-02 23:42:51 +0000

    Added style to button

--- windowsGuiTest.c
+++ windowsGuiTest.c
@@ -1,21 +1,24 @@
 #include <Windows.h>
 
-LRESULT CALLBACK WndProc(HWND handleWindow, UINT message, WPARAM wParam, LPARAM lParam);
-unsigned short InitialiseWindow(HINSTANCE *handleInstance);
-int WINAPI wWinMain(HINSTANCE handleInstance, HINSTANCE previousInstance, LPWSTR lpCommandLine, int nCommandShow)
+LRESULT CALLBACK MainWindow(HWND handle_window, UINT message, WPARAM w_param, LPARAM l_param);
+unsigned short InitialiseWindow(HINSTANCE *handle_instance);
+HBRUSH CreateAssignBrush(COLORREF colour, LPDRAWITEMSTRUCT item);
+
+int WINAPI wWinMain(HINSTANCE handle_instance, HINSTANCE previousInstance, LPWSTR lpCommandLine, int n_command_show)
 {
-    // cl windows.c user32.lib
-    MessageBoxExW(0, L"Test\n", L"Testing\n", MB_OK, 0);
-    unsigned short registerClass = InitialiseWindow(&handleInstance);
-    if(registerClass == 0){
+    // cl windows.c user32.lib kernel32.lib gdi32.lib
+    // MessageBoxExW(0, L"Test\n", L"Testing\n", MB_OK, 0);
+    unsigned short register_class = InitialiseWindow(&handle_instance);
+    if(register_class == 0){
         return 1;
     }
     // CreateWindowW(lpClassName,lpWindowName,dwStyle,x,y,nWidth,nHeight,hWndParent,hMenu,hInstance,lpParam)
     // HWND handleWindow = CreateWindowW(L"Window\0", L"Hi\0", WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, handleInstance, 0);
-    HWND handleWindow = CreateWindowExW(0L, L"Window", L"Hello", (0x00000000L | 0x00C00000L | 0x00080000L | 0x00040000L | 0x00020000L | 0x00010000L), 100, 100,500, 500, NULL, NULL, handleInstance, 0);
     
-    ShowWindow(handleWindow, nCommandShow);
-    UpdateWindow(handleWindow);
+    HWND handle_window = CreateWindowExW(0L, L"Window", L"Hello", (0x00000000L | 0x00C00000L | 0x00080000L | 0x00040000L | 0x00020000L | 0x00010000L), 100, 100,500, 500, NULL, NULL, handle_instance, 0);
+    // HWND button_window = CreateWindowExW(0L, L"BUTTON", L"Click", WS_TABSTOP | WS_VISIBLE| WS_CHILD| BS_DEFPUSHBUTTON | BS_OWNERDRAW, 10, 10, 100, 100, handle_window, (HMENU)101, 0, NULL);
+    ShowWindow(handle_window, n_command_show); 
+    UpdateWindow(handle_window);
     MSG message = {0};
 
     while (GetMessageW(&message, NULL, 0, 0))
@@ -26,52 +29,89 @@ int WINAPI wWinMain(HINSTANCE handleInstance, HINSTANCE previousInstance, LPWSTR
 
     return 0;
 }
-unsigned short InitialiseWindow(HINSTANCE *handleInstance)
+unsigned short InitialiseWindow(HINSTANCE *handle_instance)
 {
-    WNDCLASSEXW windowClass = {0};
-    windowClass.cbSize = sizeof(WNDCLASSEX);
-    windowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
-    windowClass.lpfnWndProc = WndProc;
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    windowClass.lpszMenuName = L"Hi";
-    windowClass.lpszClassName = L"Window";
-    windowClass.hIconSm = NULL;
-    windowClass.hInstance = *handleInstance;
-    windowClass.hIcon = NULL;
-    windowClass.cbClsExtra = 0;
-    windowClass.cbWndExtra = 0;
-    return RegisterClassExW(&windowClass);
+    WNDCLASSEXW window_class = {0};
+    window_class.cbSize = sizeof(WNDCLASSEX);
+    window_class.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+    window_class.lpfnWndProc = MainWindow;
+    window_class.hCursor = LoadCursor(NULL, IDC_ARROW);
+    window_class.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    window_class.lpszMenuName = L"Hi";
+    window_class.lpszClassName = L"Window";
+    window_class.hIconSm = NULL;
+    window_class.hInstance = *handle_instance;
+    window_class.hIcon = NULL;
+    window_class.cbClsExtra = 0;
+    window_class.cbWndExtra = 0;
+    return RegisterClassExW(&window_class);
 }
-LRESULT CALLBACK WndProc(HWND handleWindow, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK MainWindow(HWND handle_window, UINT message, WPARAM w_param, LPARAM l_param)
 {
     LRESULT result = 0;
     switch (message)
     {
-        case WM_COMMAND:
-        {
-            OutputDebugString("WM_COMMAND\n");
-            result = 0;
-        }
-        break;
+        case WM_CREATE:
+            {
+                // HWND button_window = CreateWindowExW(0L, L"BUTTON", L"Click", WS_TABSTOP | WS_VISIBLE| WS_CHILD| BS_DEFPUSHBUTTON | BS_OWNERDRAW, 10, 10, 100, 100, handle_window, (HMENU)101, 0, NULL);
+                // HDC button_handle = GetDC(button_window);
+                // LPDRAWITEMSTRUCT p_item_struct = (LPDRAWITEMSTRUCT) l_param;
+                // HBRUSH brush = CreateSolidBrush(RGB(252,120,31));
+                // SelectObject(button_handle, brush);
+                
+                HWND button_window = CreateWindowExW(0L, L"BUTTON", L"Click", WS_TABSTOP | WS_VISIBLE| WS_CHILD| BS_DEFPUSHBUTTON | BS_OWNERDRAW, 10, 10, 100, 100, handle_window, (HMENU)101, 0, NULL);
+                
+            }
+            break;
         case WM_ACTIVATEAPP:
-        {
-            HWND buttonHandle = CreateWindowExW(0L, L"BUTTON", L"Click", WS_TABSTOP | WS_VISIBLE| WS_CHILD| BS_DEFPUSHBUTTON, 10, 10, 100, 100, handleWindow, NULL, 0, NULL);
-            OutputDebugString("WM_ACTIVATEAPP");
-            result = 0;
-        }
-        break;
+            {
+                
+                // UpdateWindow(handle_window);
+
+                OutputDebugString("WM_ACTIVATEAPP");
+                result = 0;
+            }
+            break;
         case WM_PAINT:
-        {
-            PAINTSTRUCT paintStruct = {0};
-            RECT rectangle = {0};
-            HDC handleDCBeginPaint = BeginPaint(handleWindow, &paintStruct);
-            GetClientRect(handleWindow, &rectangle);
-            // FillRect(handleDCBeginPaint,&rectangle,0x00A12345);
-            EndPaint(handleWindow, &paintStruct);
-            result = 0;
-        }
-        break;
+            {
+                // LPDRAWITEMSTRUCT pItemStruct = (LPDRAWITEMSTRUCT) lParam;
+                PAINTSTRUCT paint_struct = {0};
+                RECT rectangle = {0};
+                HDC handle_window_hdc = BeginPaint(handle_window, &paint_struct);
+                GetClientRect(handle_window, &rectangle);
+                FillRect(handle_window_hdc,&rectangle,CreateSolidBrush(RGB(245,245,245)));
+                EndPaint(handle_window, &paint_struct);
+                result = 0;
+            }
+            break;
+        case WM_DRAWITEM:
+            {
+                RECT rectangle = {0};
+                HWND button_window = FindWindowExW(handle_window,NULL,L"BUTTON",NULL);
+                switch(LOWORD(w_param)){
+                    case 101:
+                        {
+                            GetClientRect(button_window,&rectangle);
+                            HDC button_hdc = GetDC(button_window);
+                            FillRect(button_hdc, &rectangle, CreateSolidBrush(RGB(252,120,31))); 
+                        }
+                        break;
+                } 
+            }
+            break;
+        
+        case WM_COMMAND:
+            {
+                OutputDebugString("WM_COMMAND\n");
+                switch(LOWORD(w_param)){
+                    case 101:
+                        {
+                            SendMessage(handle_window, WM_CLOSE, 0,0);
+                        }
+                }
+                result = 0;
+            }
+            break;
         case WM_DESTROY:
         {
             result = 0;
@@ -85,7 +125,20 @@ LRESULT CALLBACK WndProc(HWND handleWindow, UINT message, WPARAM wParam, LPARAM 
         }
         break;
         default:
-            result = DefWindowProc(handleWindow, message, wParam, lParam);
+            result = DefWindowProc(handle_window, message, w_param, l_param);
     }
     return result;
+}
+
+HBRUSH CreateAssignBrush(COLORREF colour, LPDRAWITEMSTRUCT item) {
+    HDC create_compatible_dc = CreateCompatibleDC(item->hDC);
+    RECT button = {0};
+    button.bottom = item->rcItem.bottom;
+    button.left = item->rcItem.left;
+    button.top = item->rcItem.top;
+    button.right = item->rcItem.right;
+    HBRUSH brush = CreateSolidBrush(colour);
+    FillRect(create_compatible_dc, &button, brush);
+    DeleteObject(create_compatible_dc);
+    return brush;
 }
